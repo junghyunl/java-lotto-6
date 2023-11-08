beadbf5bdbdd4ac31fe1bf79b0ed79ef2fa9f228
junghyun <fnsk1256@naver.com>
2023-11-08 23:16:29 +0900

    feat: 보너스 번호 입력 기능 추가

--- src/main/java/lotto/Player.java
+++ src/main/java/lotto/Player.java
@@ -7,15 +7,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
     private List<Integer> winningNumbers;
+    private int bonusNumber;
 
     public void enterWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
 
-        String input = Console.readLine();
-        String[] numbers = input.split(",");
+        String[] numbers = Console.readLine().split(",");
 
         winningNumbers = Arrays.stream(numbers)
             .map(Integer::parseInt)
             .collect(Collectors.toList());
+    }
+    public void enterBonusNumber() {
+        System.out.println("보너스 번호를 입력하세요:");
+
+        bonusNumber = Integer.parseInt(Console.readLine());
     }
 }
